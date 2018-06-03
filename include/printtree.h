370d695f95f75ed0f57efa1a565ff4b6faf1c5d6
Lppy <376286605@qq.com>
2018-06-04 02:58:26 +0800

    print tree

---
+++ include/printtree.h
@@ -0,0 +1,11 @@
+#ifndef PRINTTREE_H_
+#define PRINTTREE_H_
+
+#include "util.h"
+#include "symbol.h"
+#include "temp.h"
+#include "tree.h"
+
+void printStmList(FILE *out, T_stmList stmList);
+
+#endif
